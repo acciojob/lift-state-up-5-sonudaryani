7f8025b569a1d886366ee7ec430221c0932541e7
= <=>
2023-08-12 06:34:04 +0530

    update v1.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,40 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <ParentComponent />
+    </div>
+  )
+}
+
+const ParentComponent = () => {
+  let [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <div>
+      {isLoggedIn ? <p>You are logged in!</p> : <ChildComponent setIsLoggedIn={setIsLoggedIn} />}
+    </div>
+  )
+}
+
+const ChildComponent = ({setIsLoggedIn}) => {
+  const handleClick = () => {
+    setIsLoggedIn(true);
+  }
+  return (
+    <div>
+      <form>
+        <label>Username: </label>
+        <input type='text' /><br/>
+        <label>Password: </label>
+        <input type='password' /><br/>
+        <button type='submit' onClick={handleClick}>Login</button>
+      </form>
     </div>
   )
 }
 
 export default App
+
